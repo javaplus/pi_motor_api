b62f936538e5f84972d7d50c04e71fa4448b22fb
Barry Tarlton <btarlton@gmail.com>
2018-02-09 02:16:02 +0000

    pwm test

--- pwm_test.py
+++ pwm_test.py
@@ -1,25 +1,35 @@
 import RPi.GPIO as GPIO   # Import the GPIO library.
 import time               # Import time library
 
+
 GPIO.setmode(GPIO.BOARD)  # Set Pi to use pin number when referencing GPIO pins.
                           # Can use GPIO.setmode(GPIO.BCM) instead to use 
                           # Broadcom SOC channel names.
 
-GPIO.setup(12, GPIO.OUT)  # Set GPIO pin 12 to output mode.
-pwm = GPIO.PWM(12, 100)   # Initialize PWM on pwmPin 100Hz frequency
 
+GPIO.setup(32, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+pwm = GPIO.PWM(32, 500)   # Initialize PWM on pwmPin 100Hz frequency
+
+GPIO.setup(29, GPIO.OUT)  ## Setup GPIO Pin 11(motor a enable) to OUT
+GPIO.setup(31, GPIO.OUT)  ## Setup GPIO Pin 11(motor a enable) to OUT
+
+GPIO.output(29, True)
+GPIO.output(31, True)
 
 # main loop of program
 print("\nPress Ctl C to quit \n")  # Print blank line before and after message.
 dc=0                               # set dc variable to 0 for 0%
-pwm.start(dc)                      # Start PWM with 0% duty cycle
-while True:                        # Loop until Ctl C is pressed to stop.
-  for dc in range(0, 101, 5):      # Loop from 0 to 100 stepping dc up by 5 each loop
+pwm.start(90)                      # Start PWM with 0% duty cycle
+#while True:                        # Loop until Ctl C is pressed to stop.
+for dc in range(0, 101, 5):      # Loop from 0 to 100 stepping dc up by 5 each loop
     pwm.ChangeDutyCycle(dc)
+    #GPIO.output(32, True);
+    
     time.sleep(0.05)               # wait for .05 seconds at current LED brightness level
     print(dc)
-  for dc in range(95, 0, -5):      # Loop from 95 to 5 stepping dc down by 5 each loop
+for dc in range(95, 0, -5):      # Loop from 95 to 5 stepping dc down by 5 each loop
     pwm.ChangeDutyCycle(dc)
+    #GPIO.output(32, True)
     time.sleep(0.05)               # wait for .05 seconds at current LED brightness level
     print(dc)
  
