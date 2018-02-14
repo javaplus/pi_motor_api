4b0775845871ed7df8866b4cc2ca05963b2a7f74
Barry Tarlton <btarlton@gmail.com>
2018-02-13 19:37:31 -0500

    add Cors

--- motor_api.py
+++ motor_api.py
@@ -1,15 +1,20 @@
 import serial
 import time
 from flask import Flask
+from flask_cors import CORS
+from flask import Response
+
 app = Flask(__name__)
+CORS(app)
+
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 
-@app.route("/")
+@app.route("/test")
 def hello():
-    ser.write('0')
-    return "Hello World!"
+    #ser.write('0')
+    return Response('{"name" : "value"}', mimetype='application/json')
 
 @app.route('/forward/<string:speed>')
 def go_forward(speed):
